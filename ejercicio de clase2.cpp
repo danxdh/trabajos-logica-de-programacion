77f9b52daeac3738fa469aef105a6151e21d6468
danxdh <danielarenas721985@gmail.com>
2026-09-14 11:28:35 -0500

    agrego ejercicios de clase1

---
+++ ejercicio de clase2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char caracter1, caracter2;
+
+    cout << "Ingrese el primer caracter: ";
+    cin >> caracter1;
+
+    cout << "Ingrese el segundo caracter: ";
+    cin >> caracter2;
+
+    if (caracter1 == caracter2) {
+        cout << "Los caracteres son iguales";
+    } else {
+        cout << "Los caracteres son diferentes";
+    }
+
+    return 0;
+}
